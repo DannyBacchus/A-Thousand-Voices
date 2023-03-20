740d97f9d8d35bd54239eb75a5d5bfca3f1cdc4d
DannyBacchus <128395337+DannyBacchus@users.noreply.github.com>
2023-03-20 14:39:42 +0000

    Add files via upload

---
+++ scripts/pageHeight.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+var $j = jQuery.noConflict();
+
+     $j(document).ready(function() {
+           $j(".bodyPress").height($j(document).height());
+		   $j(".bodyInstallation").height($j(document).height());
+		   $j(".bodyPaintings").height($j(document).height());
+     });
